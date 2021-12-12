8d9614bca04acb1cba4fff2afd864216d607293c
rachelewoodcock <90653302+rachelewoodcock@users.noreply.github.com>
2021-12-12 14:38:55 -0500

    Add files via upload

---
+++ prompt.js
@@ -0,0 +1,4 @@
+var person = prompt("Welcome to the site! Whats your name?");
+
+var response = prompt("Hi " + person + " are you ready to see all the cool stuff I've been working on?");
+console.log(response);
